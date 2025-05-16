52cbf55e64a76db876ee436b24525640c4eba308
ChrisLubin <chrisx700@gmail.com>
2025-05-16 17:21:00 -0400

    added player movement

---
+++ Assets/Chris' Folder/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Time.deltaTime * new Vector3(0f, movementSpeed, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Time.deltaTime * new Vector3(movementSpeed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Time.deltaTime * new Vector3(0f, -movementSpeed, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Time.deltaTime * new Vector3(-movementSpeed, 0f, 0f);
+        }
+    }
+}
