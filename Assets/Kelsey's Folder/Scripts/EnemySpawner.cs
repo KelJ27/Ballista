a6f986c5a864378e62c8a3c8014221b66ec3dea4
KelJ27 <36095100+KelJ27@users.noreply.github.com>
2025-05-16 17:20:59 -0400

    create enemy spawner

--- Assets/Kelsey's Folder/Scripts/EnemySpawner.cs
+++ Assets/Kelsey's Folder/Scripts/EnemySpawner.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] Transform enemyPrefab;
     int x = 0;
     int y = 0;
-    
+
     void Start()
     {
+        Instantiate(enemyPrefab, new Vector3(0,1,0), Quaternion.identity);
+        Instantiate(enemyPrefab, new Vector3(0,2,0), Quaternion.identity);
+        Instantiate(enemyPrefab, new Vector3(0,3,0), Quaternion.identity);
+        Instantiate(enemyPrefab, new Vector3(0,4,0), Quaternion.identity);
+        Instantiate(enemyPrefab, new Vector3(0,5,0), Quaternion.identity);
         
     }
 
