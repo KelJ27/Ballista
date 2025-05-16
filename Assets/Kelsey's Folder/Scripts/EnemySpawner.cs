87cffcbe201df6519826dcb01b04cd851c722edf
KelJ27 <36095100+KelJ27@users.noreply.github.com>
2025-05-16 17:06:15 -0400

    refactor folder structure

---
+++ Assets/Kelsey's Folder/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    int x = 0;
+    int y = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
