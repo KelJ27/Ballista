b254b4f9f2228a158e389e03fd7f9a7d1e752753
KelJ27 <36095100+KelJ27@users.noreply.github.com>
2025-05-16 20:24:58 -0400

    implement basic random spawning in random outside the player viewport

--- Assets/Kelsey's Folder/Scripts/EnemySpawner.cs
+++ Assets/Kelsey's Folder/Scripts/EnemySpawner.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private enum SpawnSide
+    {
+        Up = 1,
+        Down = 2,
+        Left = 3,
+        Right = 4
+    }
+
     [SerializeField] Transform spawner;
     [SerializeField] Transform enemyPrefab;
     [SerializeField] Transform player;
-    int x = 0;
-    int y = 0;
+    private const float cameraBoundsX = 10f;
+    private const float cameraBoundsY = 6f;
 
     private void Start()
     {
@@ -20,10 +28,24 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, new Vector3(Random.Range(-3f, 3f),Random.Range(-3f, 3f),0), Quaternion.identity, spawner);
-        Instantiate(enemyPrefab, new Vector3(Random.Range(-3f, 3f),Random.Range(-3f, 3f),0), Quaternion.identity, spawner);
-        Instantiate(enemyPrefab, new Vector3(Random.Range(-3f, 3f),Random.Range(-3f, 3f),0), Quaternion.identity, spawner);
-        Instantiate(enemyPrefab, new Vector3(Random.Range(-3f, 3f),Random.Range(-3f, 3f),0), Quaternion.identity, spawner);
-
+        int spawnSide = Random.Range(1, 4);
+        switch (spawnSide)
+        {
+            case (int)SpawnSide.Up:
+                Instantiate(enemyPrefab, new Vector3(Random.Range(-15f, 15f), cameraBoundsY, 0), Quaternion.identity, spawner);
+                break;
+            case (int)SpawnSide.Down:
+                Instantiate(enemyPrefab, new Vector3(Random.Range(-15f, 15f), -cameraBoundsY, 0), Quaternion.identity, spawner);
+                break;
+            case (int)SpawnSide.Left:
+                Instantiate(enemyPrefab, new Vector3(-cameraBoundsX, Random.Range(-15f, 15f), 0), Quaternion.identity, spawner);
+                break;
+            case (int)SpawnSide.Right:
+                Instantiate(enemyPrefab, new Vector3(cameraBoundsX, Random.Range(-15f, 15f), 0), Quaternion.identity, spawner);
+                break;
+            default:
+                break;
+        }
     }
+
 }
