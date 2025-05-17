85483b9c431acf7eefee57eb1cf470efb4b0c18b
KelJ27 <36095100+KelJ27@users.noreply.github.com>
2025-05-17 01:29:05 -0400

    Implement enemy taking damage

--- Assets/Kelsey's Folder/Scripts/Enemy.cs
+++ Assets/Kelsey's Folder/Scripts/Enemy.cs
@@ -1,23 +1,72 @@
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 2f;
-    
-    PlayerController target = PlayerController.Player;
+    [SerializeField] private float maxHealth = 10;
+    [SerializeField] private int atk = 2;
+
+    [SerializeField] private Image healthBar;
+    private SpriteRenderer sr;
+
+    private PlayerController target = PlayerController.Player;
+    private float currentHealth;
 
     private void Start()
     {
-
+        sr = this.GetComponent<SpriteRenderer>();
+        currentHealth = maxHealth;
     }
 
     private void Update()
     {
         MoveTowardsPlayer();
+        //FlashDamageColor();
+    
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage();
+        }
     }
 
     private void MoveTowardsPlayer()
     {
         this.transform.position = Vector2.MoveTowards(this.transform.position, this.target.transform.position, this.movementSpeed * Time.deltaTime);
     }
+
+    private async Task FlashDamageColor()
+    {
+        this.sr.color = Color.red;
+        await UniTask.WaitForSeconds(0.05f);
+        this.sr.color = Color.white;
+        
+    }
+
+    private void DecreaseHealthBar()
+    {
+        healthBar.fillAmount = currentHealth / maxHealth;
+    }
+
+    public async void TakeDamage()
+    {
+        currentHealth -= 2;
+
+        await FlashDamageColor();
+        DecreaseHealthBar();
+
+        if (this.currentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DamagePlayer()
+    {
+
+    }
+
+
 }
