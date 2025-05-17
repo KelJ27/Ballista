54acf50e083aea16c85e3236e25283dfee59d7c9
ChrisLubin <chrisx700@gmail.com>
2025-05-17 00:27:57 -0400

    Added bullet container

--- Assets/Chris' Folder/Scripts/GunController.cs
+++ Assets/Chris' Folder/Scripts/GunController.cs
@@ -5,10 +5,12 @@ public class GunController : MonoBehaviour
     [SerializeField] private float _distanceFromPlayer = 0.75f;
     [SerializeField] private Transform _bulletPrefab;
     [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Transform _bulletContainer;
     [SerializeField] private int _rateOfFirePerSecond = 50;
     private float _minTimeBetweenEachShot => 1000 / this._rateOfFirePerSecond;
     private float _timeSinceLastShot = float.MaxValue;
     private bool _canShoot => this._timeSinceLastShot >= this._minTimeBetweenEachShot;
+
 
     private void Update()
     {
@@ -24,7 +26,7 @@ public class GunController : MonoBehaviour
 
     private void Shoot()
     {
-        BulletController bullet = Instantiate(this._bulletPrefab, this._shootPoint.position, Quaternion.identity).GetComponent<BulletController>();
+        BulletController bullet = Instantiate(this._bulletPrefab, this._shootPoint.position, Quaternion.identity, this._bulletContainer).GetComponent<BulletController>();
         bullet.SetAimDirection(Helpers.GetDirectionTowardsCursor(transform.position));
         this._timeSinceLastShot = 0;
     }
